c78d2dc7688be0347e261760597ad8caea947c97
DeVaris Brown <devaris@devaris.com>
2013-10-07 17:55:13 -0700

    add home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Pinteresting::Application.routes.draw do
+  root to: "pages#home"
+
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
